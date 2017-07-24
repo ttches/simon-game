40b930b043d49652262d6970f4cb1f2369f63267
Tyler Ches <ttches@gmail.com>
2017-07-23 19:57:52 -0700

    Made height / width dynamic

--- src/index.js
+++ src/index.js
@@ -6,3 +6,22 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const board = document.querySelector('#board');
+const width = board.offsetWidth;
+
+window.addEventListener('resize', dynamicHeight);
+
+dynamicHeight();
+function dynamicHeight() {
+  const windowWidth = window.innerWidth;
+  if (windowWidth < 400) {
+    const tiles = document.querySelectorAll('.tile');
+    tiles.forEach((tile) => {
+      console.log(tile.offsetWidth);
+      tile.style.height = `${windowWidth * .5}px`;
+    });
+  } else {
+    return;
+  }
+}
